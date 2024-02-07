5f731536e5e1bfd993e880f798d644314df2a19b
WooDMaNbtw <nikita.profatilov@gmail.com>
2024-02-07 12:03:31 +0400

    Main component App changed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import Counter from "./components/Counter";
+import React, {useState} from "react";
+import './styles/App.css';
+import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
+import PostForm from "./components/PostForm";
+
 
 function App() {
+    const [posts, setPosts] = useState([
+        {id: 1, title: "Javascript", body: "Description"},
+        {id: 2, title: "Python", body: "Description"},
+        {id: 3, title: "C#", body: "Description"},
+        {id: 4, title: "TypeScript", body: "Description"}
+    ])
+
+
+    function addNewPost(e) {
+        e.preventDefault()
+        setPosts([...posts, {...post, id: Date.now()}])
+        setPost({title: '', body: ''})
+    }
+
   return (
     <div className="App">
-      <Counter/>
-      <Counter/>
-      <Counter/>
-      <Counter/>
-      <Counter/>
+        <PostForm/>
+        <PostList posts={posts} title="Список основных постов"/>
     </div>
   );
 }
